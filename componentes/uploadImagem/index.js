2680f91323a8e6463b15961b0b8a13ccc3ebbcd7
Lucas Ramos <lucas.alves.r30@gmail.com>
2023-06-25 00:36:40 -0300

    componentes de botao avatar, upload de imagem

--- componentes/uploadImagem/index.js
+++ componentes/uploadImagem/index.js
@@ -1,5 +1,63 @@
-export default function UploadImagem(){
+/* eslint-disable @next/next/no-img-element */
+import { useEffect, useRef } from "react";
+
+export default function UploadImagem({
+    className='',
+    setImagem,
+    imagemPreview,
+    imagemPreviewClassName='null',
+    aoSetarAReferencia='null'
+}){
+    const referenciaInput = useRef(null);
+
+    useEffect(() => {
+        if(!aoSetarAReferencia){
+            return;
+        }
+            aoSetarAReferencia(referenciaInput?.current);
+
+    }, [referenciaInput?.current]);
+
+    const abrirSeletorContainer = () => {
+        referenciaInput?.current?.click();
+    }
+
+    const aoAlterarImagem = () => {
+        console.log('aoAlterarImagem');
+
+        if(!referenciaInput?.current?.files?.length){
+            return;
+        }
+            const arquivo = referenciaInput?.current?.files[0];
+            const fileReader = new FileReader();
+            fileReader.readAsDataURL(arquivo); //read a file and returns a URL
+            fileReader.onloadend = () => {
+                setImagem({
+                    preview: fileReader.result,
+                    arquivo,
+                });
+
+            }
+    }
+    
     return(
-        <input type="file" className="oculto" accept="Image/*" />
+        <div className={`uploadImagemContainer ${className}`} onClick={abrirSeletorContainer}>
+            {imagemPreview && (
+                <div className="imagemPreviewContainer">
+                    <img
+                        src={imagemPreview}
+                        alt="Imagem preview"
+                        className={imagemPreviewClassName}
+                    />
+                </div>
+            )}
+            <input 
+                type="file" 
+                className="oculto" 
+                accept="Image/*"
+                ref={referenciaInput}
+                onChange={aoAlterarImagem} 
+            />
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,31 @@
 import Avatar from "@/componentes/avatar";
 import Botao from "@/componentes/botao";
 import UploadImagem from "@/componentes/uploadImagem";
+import { useRef, useState } from "react";
 
 export default function Home() {
+
+  const [imagem, setImagem] = useState(null);
+  const referenciaInput = useRef(null);
+
   return (
     <div>
       <h1>Ola Mundo!</h1>
-      <UploadImagem />
+
+      <button onClick={() => referenciaInput?.current.click()}>abrir seletor de arquivos</button>
+
+      <UploadImagem 
+        setImagem={setImagem} 
+        imagemPreview={imagem?.preview}
+        aoSetarAReferencia={(ref) => referenciaInput.current = ref}  
+      />
+
       <Avatar />
+
       <Botao texto={'Login'} cor="primaria" desabilitado={false} manipularClique={() => {
         console.log('Botao clicado!')
-      }}/>
+      }} />
+
     </div>
   )
 }
